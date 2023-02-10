8b43ed656aabffdd82840b375feb1beaf8e66b15
Batan Andrei <yabatan777@gmail.com>
2023-02-10 09:45:04 +0300

    initial commit

---
+++ JS.js
@@ -0,0 +1,16 @@
+const apiKey = "da102ad5";
+const apiUrl = "https://www.omdbapi.com/";
+
+const form = document.querySelector('.searchForm');
+form.addEventListener('submit', submitForm);       //вешаем обработчик на всю форму(form) событие submit
+
+function submitForm(e){
+    e.preventDefault();  // отключает дефолтные настройки, в данном случае отключает перезагрузку страницы при submit.
+    const searchValue = form.querySelector('#searchInput').value;
+    fetch(`${apiUrl}?apikey=${apiKey}&s=${searchValue}`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+      });
+
+}
